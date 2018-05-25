a93fdb1a04342d2aef6a8be9efac029d02a405b3
Jules Roman Polidario <jrpolidario@gmail.com>
2018-05-25 17:47:48 +0100

    update to conform to deprecation-warning changes

--- config.ru
+++ config.ru
@@ -5,6 +5,8 @@ Bundler.require :default, :development
 require 'live_record'
 require 'action_cable/engine'
 
-Combustion.initialize! :all
+Combustion.initialize! :all do
+  config.active_record.sqlite3.represent_boolean_as_integer = true
+end
 
 run Combustion::Application
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,9 @@ Bundler.require :default, :development
 require 'live_record'
 require 'action_cable/engine'
 
-Combustion.initialize! :all
+Combustion.initialize! :all do
+  config.active_record.sqlite3.represent_boolean_as_integer = true
+end
 
 require 'rspec/rails'
 require 'capybara/rails'
